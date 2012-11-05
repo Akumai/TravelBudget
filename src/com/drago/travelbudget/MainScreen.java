9b04534d05cf35f587e15beb0af4aad99d7a2ae5
Akumai <drago@bk.ru>
2012-11-05 11:05:28 +0400

    Two buttons

--- src/com/drago/travelbudget/MainScreen.java
+++ src/com/drago/travelbudget/MainScreen.java
@@ -2,8 +2,16 @@ package com.drago.travelbudget;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.EventObject;
 
 public class MainScreen extends Activity {
+    public static int counter = 0;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +19,14 @@ public class MainScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void newTravelBtn_Click(View v){
+        Button b = new Button(MainScreen.this);
+
+        b.setText("Button " + counter++);
+        b.setWidth(200);
+        b.setHeight(30);
+        b.buildLayer();
     }
 }
